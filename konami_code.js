9866e8654144fd34ab3f60b664aeb743052d29a6
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-22 21:43:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ var index = 0
 
 function init() {
   // your code here
-  document.body.addEventListener("keydown", function(event) {
+  document.body.addEventListener("keydown", function(e) {
   const key = e.key;
     if (key === codes[index]) {
       index++;
