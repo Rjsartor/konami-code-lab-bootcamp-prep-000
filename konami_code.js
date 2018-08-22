4d4fc19840f18648eee93a83016f63e046a6fc64
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-22 20:02:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+var index = 0
+
 function init() {
   // your code here
+   var body = document.body;
+  body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which);
+     // check if key is equal to current value of index in code array
+    // then loop through the array if conditions match
+     if (key === code[index]) {
+      // if yes, increment index by 1
+      index++;
+       // if still in the loop, check if index is equal to code array length
+      if (index === code.length - 1) {
+        // reset the index counter
+        index = 0;
+        // if yes, you passed, congrats!
+        alert("Congrats!");
+      }
+   // if key doesn't match current index in code array, then reset the index to zero
+  } else {
+    index = 0;
+  }
+   });
 }
