5a606ee71fe263396f5e54425e9253b5f1e73ba0
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-22 21:11:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,5 +22,8 @@ function init() {
       if ( keys.length > codes.length ) {
             keys.shift();
           }
+      if ( keys.toString() == code.toString() ) {
+            $('#monster-container').slideDown('slow');
+          }
   })
 }
