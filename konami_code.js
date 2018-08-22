b2a9d3bc9c8bfc67cddd4540c85a6ea2c54b6d7f
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-22 23:22:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,22 @@
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
 
 function init() {
-  var index = 0
-  // your code here
-  document.body.addEventListener("keydown", function(e) {
-    function onKeyDownHandler(e) {
-  const key = e.key;
+  let index = 0;
+  const input = document.querySelector('input')
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key
     if (key === codes[index]) {
       index++;
       if (index === codes.length) {
@@ -15,7 +26,4 @@ function init() {
     } else {
       index = 0;
     }
-  }});
-}
-init()
-
+  })
